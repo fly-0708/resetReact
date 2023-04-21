f6d0317ea85b8bfcd2a78a7b1ceef19a6129ecf4
fly <1833174181@qq.com>
2023-04-21 18:04:19 +0800

    lay

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,20 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import router from "./router/index";
+import { Grid } from "@mui/material";
+import './assets/css/index.less'
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+
+  <Grid container className="grid">
+    <Grid xs={2}>
+    </Grid>
+    <Grid xs={10}>
+      <React.StrictMode>
+        <RouterProvider router={router} />
+      </React.StrictMode>
+
+    </Grid>
+  </Grid>
+
 )
